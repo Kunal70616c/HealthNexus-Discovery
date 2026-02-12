0a558a04b1dab516ea48ae2a2cd87a9a5242d997
raiko <kunal.cs.dev@outlook.com>
2026-02-12 20:57:37 +0530

    Service Discovery By Eureka Server

---
+++ src/test/java/sh/surge/kunal/healthnexus/EurekadiscoveryserverApplicationTests.java
@@ -0,0 +1,13 @@
+package sh.surge.kunal.healthnexus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekadiscoveryserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
